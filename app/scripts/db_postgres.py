f0c13e66e5773128f484360976d3097fc2b6bd7a
emmafer <emmafer@gmail.com>
2022-11-23 00:04:11 -0300

    backend form fix

--- app/scripts/db_postgres.py
+++ app/scripts/db_postgres.py
@@ -1,4 +1,4 @@
-from peewee import PostgresqlDatabase, Model, CharField, ForeignKeyField, IntegerField, IntegrityError
+from peewee import PostgresqlDatabase, Model, CharField, IntegerField, IntegrityError
 import os
 
 DBNAME = os.environ["DBNAME"]
@@ -15,23 +15,36 @@ class sup_db(Model):
         database = db
 
 class Usuario(sup_db):
+    grupo_sup = IntegerField()
     nombre = CharField()
     apellido = CharField()
     edad = IntegerField()
     email = CharField(primary_key = True)
-
-class Testerino_rel(sup_db):
-    input2 = CharField()
+    nacionalidad = CharField()
+    pais_residencia = CharField()
+    ocupacion = CharField()
+    dispositivo = CharField()
+    mic_y_cam = CharField()
+    funcion_sup = CharField()
+    gustos_sup = CharField()
 
 def create_user(lista):
     db.connect()
     try:
         with db.atomic():
             Usuario.create(
-                nombre = lista[0],
-                apellido = lista[1],
-                edad = lista[2],
-                email = lista[3],
+                grupo_sup = lista[0],
+                nombre = lista[1],
+                apellido = lista[2],
+                edad = lista[3],
+                email = lista[4],
+                nacionalidad = lista[5],
+                pais_residencia = lista[6],
+                ocupacion = lista[7],
+                dispositivo = lista[8],
+                mic_y_cam = lista[9],
+                funcion_sup = lista[10],
+                gustos_sup = lista[11]
             )
     except IntegrityError:
         print(f'¡Ese correo ya se encuentra registrado! Por favor, intente nuevamente con un nuevo correo.')
