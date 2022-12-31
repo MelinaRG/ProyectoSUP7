ef0c4bc28651b07ad9e4fa85ddf719a24707885c
Emmanuel <emmanuel.ac6@gmail.com>
2022-12-31 12:28:53 -0300

    Delete 1 Mi Grupo.py

--- pages/1 Mi Grupo.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title='Mi cohorte', 
-                   page_icon='📋', 
-                   layout="centered", 
-                   initial_sidebar_state="collapsed", 
-                   menu_items=None)
